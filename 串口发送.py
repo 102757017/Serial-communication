c6beee1b36968ad98aa18e9daaa5d86de14f39f2
102757017 <sdf63fg@yeah.net>
2019-12-30 20:40:13 +0800

    update：从配置文件内加载串口配置信息
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 串口发送.py
+++ 串口发送.py
@@ -2,12 +2,43 @@
 # -*- coding: UTF-8 -*-
 import serial
 import time
+import configparser
+import os
 
 
-#实例化串口号、波特率、等待时间
-#timeout=None 永远等待，直到有数据传过来（阻塞）  
-#timeout=0 不等待，收不到数据直接退出读取（非阻塞） 
-ser=serial.Serial('COM2',19200,timeout=1)
+#在传递键值对数据时，会将键名 全部转化为小写
+conf = configparser.ConfigParser()
+if os.path.isfile("seting.ini"):
+    conf.read("seting.ini")
+    COM_No  = conf.get("Serial_settings", "COM_No")
+    baud=int(conf.get("Serial_settings","baud"))
+    timeout=int(conf.get("Serial_settings","timeout"))
+    bytesize=int(conf.get("Serial_settings","bytesize"))
+    stopbits=int(conf.get("Serial_settings","stopbits"))    
+    parity={"None":serial.PARITY_NONE,"EVEN":serial.PARITY_EVEN,"ODD":serial.PARITY_ODD}
+    parity=parity[conf.get("Serial_settings","parity")]
+else:
+    conf.add_section('Serial_settings')
+    conf.set('Serial_settings', 'COM_No', 'COM1')
+    conf.set('Serial_settings', "baud", '19200')
+    conf.set('Serial_settings', 'timeout', '1')
+    conf.set('Serial_settings', 'bytesize', '8')
+    conf.set('Serial_settings', 'stopbits', '1')
+    conf.set('Serial_settings', 'parity', 'None')
+    conf.write(open('seting.ini', 'w'))
+
+
+print('获取指定的section下的option', type(parity), parity)
+
+# 实例化串口号、波特率、等待时间
+ser=serial.Serial(port=COM_No,
+                  baudrate=baud,
+                  bytesize=bytesize,
+                  parity=parity,
+                  stopbits = stopbits,
+                  timeout=timeout)
+
+
 
 #打印串口信息 
 print("串口信息：",ser)
--- 串口接收.py
+++ 串口接收.py
@@ -2,6 +2,8 @@
 # -*- coding: UTF-8 -*-
 import serial
 import time
+import configparser
+import os
 
 '''
 #timeout=None 永远等待，直到有数据传过来（阻塞),timeout=0 不等待，收不到数据直接退出读取（非阻塞）
@@ -17,8 +19,35 @@ rtscts：硬件流控
 dsrdtr：硬件流控
 interCharTimeout:字符间隔超时
 '''
+#在传递键值对数据时，会将键名 全部转化为小写
+conf = configparser.ConfigParser()
+if os.path.isfile("seting.ini"):
+    conf.read("seting.ini")
+    baud=int(conf.get("Serial_settings","baud"))
+    timeout=int(conf.get("Serial_settings","timeout"))
+    bytesize=int(conf.get("Serial_settings","bytesize"))
+    stopbits=int(conf.get("Serial_settings","stopbits"))    
+    parity={"None":serial.PARITY_NONE,"EVEN":serial.PARITY_EVEN,"ODD":serial.PARITY_ODD}
+    parity=parity[conf.get("Serial_settings","parity")]
+else:
+    conf.add_section('Serial_settings')
+    conf.set('Serial_settings', "baud", '19200')
+    conf.set('Serial_settings', 'timeout', '1')
+    conf.set('Serial_settings', 'bytesize', '8')
+    conf.set('Serial_settings', 'stopbits', '1')
+    conf.set('Serial_settings', 'parity', 'None')
+    conf.write(open('seting.ini', 'w'))
+
+
+print('获取指定的section下的option', type(parity), parity)
+
 # 实例化串口号、波特率、等待时间
-ser=serial.Serial('COM1',19200,timeout=1,xonxoff=0)
+ser=serial.Serial(port="COM1",
+                  baudrate=baud,
+                  bytesize=bytesize,
+                  parity=parity,
+                  stopbits = stopbits,
+                  timeout=timeout)
 
 # 打印串口信息 
 print("串口信息：",ser)
